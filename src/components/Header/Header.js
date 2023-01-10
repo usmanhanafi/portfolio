634e7c6933716fb45856d8145b33f6332f0f186a
Usmanhanafi <tholush@gmail.com>
2023-01-11 03:47:35 +0700

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,11 +26,6 @@ const Header = () => (
           <NavLink>About</NavLink>
         </Link>
       </li>
-      <li>
-        <a href="https://blog.vipuljha.com">
-          <NavLink>Blog</NavLink>
-        </a>
-      </li>
     </Div2>
     <Div3>
       <SocialIcons href="https://github.com/usmanhanafi">
